7d387045165e68378242e0c8c6d594f9ef693dc7
bceva <115519115+bceva@users.noreply.github.com>
2023-05-11 03:05:02 -0400

    env

--- produce.js
+++ produce.js
@@ -62,8 +62,8 @@ const getProducts = async (supabase) => {
     
 const initSupabase = async () => {
 
-    const supabaseUrl = env.SUPABASE_URL;
-    const supabaseKey = env.SUPABASE_KEY;
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabaseKey = process.env.SUPABASE_KEY;
   
     const supabase = createClient(supabaseUrl, supabaseKey);
   
